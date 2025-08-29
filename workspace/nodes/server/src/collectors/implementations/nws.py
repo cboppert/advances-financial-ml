db5cd0d6b4cbf3ae78d5f005d85eeffc94d9d332
Cody Boppert <Cody.Boppert@warnermedia.com>
2025-08-29 12:55:17 -0400

    8 29 25 - Pull glossary from NWS

--- workspace/nodes/server/src/collectors/implementations/nws.py
+++ workspace/nodes/server/src/collectors/implementations/nws.py
@@ -1,3 +1,4 @@
+import requests
 import time
 
 NWS_API = "https://api.weather.gov"
@@ -6,4 +7,12 @@ def start_nws_collector():
   starttime = time.monotonic()
   while True:
     print("Conseguido el tiempo para Nueva York...")
+    getNWSData('glossary')
     time.sleep(60.0 - ((time.monotonic() - starttime) % 60.0))
+
+def getNWSData(path):
+  data = requests.get(getNWSUrl(path))
+  print(data.text)
+
+def getNWSUrl(path):
+  return "%s/%s" %(NWS_API, path)
