87d931d91490ab7eaf85d72f7314e89f91bda958
Cody Boppert <cody@codyboppert.nyc>
2025-12-21 21:38:57 -0500

    Añadir una escripta

---
+++ setup/initNano.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Una escripta simple a establecer unas cosas
+
+mv ../configs/vimrc ~/.vimrc
+cat ../configs/bashrc >> ~/.bashrc
+
+mkdir -p Documents/notes
+touch Documents/notes/scratchpad.md
