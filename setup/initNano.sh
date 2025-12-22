ef961c0d62d66210a3d37490e79a471bbe2ff16d
Cody Boppert <cody@codyboppert.nyc>
2025-12-21 21:44:59 -0500

    Añadir NERDTree

--- setup/initNano.sh
+++ setup/initNano.sh
@@ -2,6 +2,10 @@
 
 # Una escripta simple a establecer unas cosas
 
+sudo apt-get install curl
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 mv ../configs/vimrc ~/.vimrc
 cat ../configs/bashrc >> ~/.bashrc
 
